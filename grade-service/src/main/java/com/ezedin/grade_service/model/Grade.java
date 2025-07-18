94666cb20b0229143bee4559f8f32a519301e347
Ezedin Ahmed <ezedinadefa@gmail.com>
2025-07-18 07:16:15 +0300

    creating grade-service

--- grade-service/src/main/java/com/ezedin/grade_service/model/Grade.java
+++ grade-service/src/main/java/com/ezedin/grade_service/model/Grade.java
@@ -1,4 +1,22 @@
 package com.ezedin.grade_service.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Grade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long studentId;
+    private String name;
+
 }
